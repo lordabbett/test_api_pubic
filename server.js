fe8eba829191ee79263ae621ed8d10ddd36b6285
Syed Raza <sraza@lordabbett.com>
2023-12-19 17:18:42 -0500

    Test API

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
+
 var http = require('http')
+var url = require('url')
 
 var port = 4000
 
 var server = http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.end('Hello Test api - 3 Scale Testing\n')
+  var requestUrl = url.parse(request.url, true)
+
+  if(requestUrl.pathname === '/health') {
+    response.writeHead(200, {'Content-Type': 'text/plain'})
+    response.end('Server is healthy\n')
+  } else {
+    response.writeHead(200, {'Content-Type': 'text/plain'})
+    response.end('Hello Test api - 3 Scale Testing\n')
+  }
 })
 
 server.listen(port)
